9df0432cd1348f02c83804fa4b09db03180da9ae
WarreBogaerts15 <s142255@ap.be>
2025-11-06 09:19:37 +0100

    Added user and location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
-
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 }
